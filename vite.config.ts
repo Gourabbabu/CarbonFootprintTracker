d3e340f13dba8f5e7065386e7cc40d08c8f7197e
gourab <gourab11335577@gmail.com>
2025-07-17 14:19:31 +0530

    Updated package.json and vite.config.ts for Cloudflare deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : "/CarbonFootprint-Tracker",
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: "/", // <- important for Cloudflare
 });
